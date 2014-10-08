81ef9b345c8aa696110678dcc325f0c46bd89ab5
Aris Adamantiadis <aris@0xbadc0de.be>
2014-10-08 18:17:40 +0200

    Hexdump with highlight

--- tools/hexdump.py
+++ tools/hexdump.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from libctf import hexdump
+from libctf import hexdump, cdecode
 
 import argparse
 
 def main():
 	parser = argparse.ArgumentParser(description="Hexdumping a binary files")
-	parser.add_argument('-H', help='highlight', metavar="pattern")
+	parser.add_argument('-H', nargs='*', help='highlight, C encoding', metavar="pattern")
 	parser.add_argument('-o', help='start hexdumping at OFFSET', metavar="OFFSET")
 	parser.add_argument('-l', help='Limit to LINES lines', metavar="LINES")
 	parser.add_argument('file', nargs=1, metavar='FILE')
@@ -22,7 +22,10 @@ def main():
 		offset=0
 	if args.l != None:
 		data = data[:int(args.l) * 16]
-	hexdump(data, highlight=args.H, printoffset=offset)	
+	highlight = None
+	if args.H != None:
+		highlight = map(cdecode, args.H)		
+	hexdump(data, highlight=highlight, printoffset=offset)	
 
 if __name__ == '__main__':
 	main()
