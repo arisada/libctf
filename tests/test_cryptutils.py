27e118bf6547501d46efbbb512adb7b83860640f
Aris Adamantiadis <aris@0xbadc0de.be>
2016-11-22 17:45:44 +0100

    test for ASN1/PKCS1_1.5

--- tests/test_cryptutils.py
+++ tests/test_cryptutils.py
@@ -122,5 +122,55 @@ class TestCrypto(unittest.TestCase):
 		cipher = pub.encrypt(msg)
 		self.assertEqual(priv.decrypt(cipher), msg)
 
+# Using a DER-encoded RSA-SHA1 Signature as testcase
+# 3021 0x20=cons, 0x10=sequence
+#   3009
+#     0605 Object (5) d = 2b0e03021a
+#     0500 Null (0)
+#   0414 octet string d = d033e22ae348aeb5660fc2140aec35850c4da997
+class TestASN1(unittest.TestCase):
+	def testObject(self):
+		obj = ASN1(tag=ASN1.OBJECT, value=b'\x2b\x0e\x03\x02\x1a')
+		encoded = obj.encode()
+		self.assertEqual(encoded, b'\x06\x05\x2b\x0e\x03\x02\x1a')
+	def testNull(self):
+		obj = ASN1(tag=ASN1.NULL, value=None)
+		encoded = obj.encode()
+		self.assertEqual(encoded, b'\x05\x00')
+	def testOctetString(self):
+		obj = ASN1(tag=ASN1.OCTET_STRING,
+			value=hexdecode("d033e22ae348aeb5660fc2140aec35850c4da997"))
+		encoded = obj.encode()
+		self.assertEqual(encoded,
+			hexdecode("0414d033e22ae348aeb5660fc2140aec35850c4da997"))
+	def testSequence(self):
+		obj1 = ASN1(tag=ASN1.OBJECT, value=b'\x2b\x0e\x03\x02\x1a')
+		obj2 = ASN1(tag=ASN1.NULL, value=None)
+		obj = ASN1(tag=ASN1.SEQUENCE, value=(obj1, obj2))
+		encoded = obj.encode()
+		self.assertEqual(encoded, hexdecode("300906052b0e03021a0500"))
+
+
+# Using 01fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+#		fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+#		ffffffffffffffffffffffffffffffffffffffffffffffffffffffff0030213
+#		00906052b0e03021a05000414d033e22ae348aeb5660fc2140aec35850c4da9
+#		97
+# =
+# 548612406879368868325593625118720927007439263593233207011200198845619
+# 738175967294716517569953636279361328472533787211174495818386274464790
+# 322410371824567029961449870071000626453559019779193402464151254126235
+# 979519159345697090595016758743143497910662445585738042643543839309893
+# 2179697773470171542105991575
+# as a test signature
+
+class TestPKCS115(unittest.TestCase):
+	def testSign(self):
+		data = hexdecode("3021300906052b0e03021a05000414d033e22ae34"
+			"8aeb5660fc2140aec35850c4da997")
+		signature = \
+		5486124068793688683255936251187209270074392635932332070112001988456197381759672947165175699536362793613284725337872111744958183862744647903224103718245670299614498700710006264535590197791934024641512541262359795191593456970905950167587431434979106624455857380426435438393098932179697773470171542105991575
+		encoded = pkcs115_encode(data, 1024, do_sign=True)
+		self.assertEqual(encoded, signature)
 if __name__ == '__main__':
 	unittest.main()
